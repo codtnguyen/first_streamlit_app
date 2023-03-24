8229e8a9b3ff2591ff778212f043b1efe83e11e1
codtnguyen <127454458+codtnguyen@users.noreply.github.com>
2023-03-24 10:44:28 +1030

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,5 +64,5 @@ def insert_row_snowflake(new_fruit):
     my_cur.execute("insert into fruit_load_list values (new_fruit)")
     return "Thanks for adding" + new_fruit
 
-insert_row_snowflake(newfruit)
+my_cnx.execute("insert_row_snowflake(newfruit)")
 
